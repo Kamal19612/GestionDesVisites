6125b1dacefc29622a333c747f8eab0f4d651c85
Kamal19612 <ouedraogokamalelise@gmail.com>
2026-01-16 00:05:23 +0000

    initialisation

---
+++ src/main/java/com/tech/visit_management/GestionDesVisitesApplication.java
@@ -0,0 +1,13 @@
+package com.tech.visit_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesVisitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesVisitesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tech/visit_management/GestionDesVisitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.visit_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesVisitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
